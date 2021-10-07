404d3933683b5503443611794daa4a4d8e469fdb
Aliaksei <aliakseiyudziankou@gmail.com>
2021-10-07 13:47:30 +0300

    feat: add solution task_2

---
+++ task_4/task_2.js
@@ -0,0 +1,19 @@
+console.log("--------------- task - 2 ")
+
+/* let word = "hello" */
+let word = "warfire"
+
+let solution = (param) => {
+    
+    let result = "";
+    let wordPattern = [];
+    let wordToArray = param.split('');
+    let uniqueArr = [...new Set(wordToArray)];
+           for (i = 0; i <= wordToArray.length - 1; i++) {
+        let number = uniqueArr.indexOf(wordToArray[i])
+        wordPattern.push(number);
+    }
+    result = wordPattern.join('.');
+    return result
+}
+console.log(solution(word))
