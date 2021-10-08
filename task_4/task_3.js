465243a2b27ca24ea6d1af1330fdeb49ad9ca56f
Aliaksei <aliakseiyudziankou@gmail.com>
2021-10-09 00:16:12 +0300

    feat: add solution task_3

---
+++ task_4/task_3.js
@@ -0,0 +1,29 @@
+
+console.log("------------ task_3 ")
+let str = "aaaabbcdefffffffg";
+
+let doublbrakets = (inputString) => {
+    let result = ''
+    let count = 0;
+    let strToArray = inputString.split('');
+    for (let i = 0; i <= strToArray.length - 1; i++) {
+        if (strToArray[i] === strToArray[i + 1]) {
+            count++;
+        } else {
+            if (count === 0) {
+                result += String(strToArray[i]);
+                count = 0;
+            }
+            if (count === 1) {
+                result += String(strToArray[i]).repeat(count + 1);
+                count = 0;
+            }
+            if (count >= 2) {
+                result += String(strToArray[i]).repeat(2) + '[' + String(strToArray[i]).repeat(count - 1) + "]";
+                count = 0;
+            }
+        }
+    }
+    return result
+}
+console.log(doublbrakets(str))
