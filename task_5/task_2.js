2c1fafa099d140a21ed54be590184e24cb50443c
Aliaksei <aliakseiyudziankou@gmail.com>
2021-10-10 18:06:43 +0300

    refactor: solution  task_2

--- task_5/task_2.js
+++ task_5/task_2.js
@@ -2,24 +2,19 @@
 console.log("------- task_2")
 
 function memo(fn) {
-    /*  debugger */
     return (...arg) => {
-        let cache = [[1, 2, 3], [2, 2, 4]]
+        let cache = [[1, 2, 3], [2, 2, 4]] // value for example
         for (let i = 0; i < cache.length; i++) {
             if (arg[0] === cache[i][0] || arg[0] === cache[i][1] &&
                 arg[1] === cache[i][0] || arg[1] === cache[i][1]) {
-
-                console.log('Fetching from cache');
                 return cache[i][2];
             } else {
-                console.log('Fetching from function');
-                let res = arg[0] + arg[1]
+                let res = sum(arg[0], arg[1])
                 let elem = [arg[0], arg[1], res]
                 cache.push(elem)
                 return res
             }
         }
-        /*  return arg[0] + arg[1] */
     }
 }
 
