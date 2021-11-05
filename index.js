91a5079fbc22ddff435b1bd80edd60aa9e55c639
Aliaksei <aliakseiyudziankou@gmail.com>
2021-11-05 12:54:09 +0300

    init: add webpack

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import {Calendar, monthName} from './js/calendar.js'
 import {a} from './js/back_next_onclick.js'
 console.log(a)
 
-
 initialisation()
 export let DatefromPage = new Date(Calendar.getYearContent(), monthName.indexOf(Calendar.getFullNameMonth()));
 apdateApp()
