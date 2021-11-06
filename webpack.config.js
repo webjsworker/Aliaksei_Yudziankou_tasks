0cc601f2cedec592987c16699fafc1e0360f7667
Aliaksei <aliakseiyudziankou@gmail.com>
2021-11-06 19:10:37 +0300

    refactor: webpack config, package

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].app.js',
-    },
-    devtool: 'source-map',
-    watch: true,
-    plugins: [
-        new CleanWebpackPlugin(),
-    ],
+module.exports = (env) => {
+    return {
+        entry: {
+            main: path.resolve(__dirname, './index.js'),
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].app.js',
+        },
+        /* devtool: 'source-map', */
+        watch: false,
+        plugins: [
+            new CleanWebpackPlugin(),
+            new Dotenv({
+                path: `./.env.${env}`
+            }),
+        ],
+    }
 }
