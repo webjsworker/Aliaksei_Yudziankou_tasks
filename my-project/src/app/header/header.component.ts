09d9fbfde011f79e60df9d1387c3bc087d825fa1
Aliaksei <aliakseiyudziankou@gmail.com>
2021-11-30 12:35:22 +0300

    feat: add pipes, one-way bibnding

--- my-project/src/app/header/header.component.ts
+++ my-project/src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public headerDate: Date = new Date();
+  public score: number = 10; 
+
   constructor() { }
 
   ngOnInit(): void {
