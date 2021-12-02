28ede70a9202721b56618dab2dd37029778095dc
Aliaksei <aliakseiyudziankou@gmail.com>
2021-12-02 23:08:20 +0300

    feat: add ngStyle, FormsModule

--- my-project/src/app/feetback/feetback.component.ts
+++ my-project/src/app/feetback/feetback.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Message{
+  constructor(public name: string, 
+              public message: string, 
+              public rating: string)
+  { }
+}
+
 @Component({
   selector: 'app-feetback',
   templateUrl: './feetback.component.html',
   styleUrls: ['./feetback.component.scss']
 })
 export class FeetbackComponent implements OnInit {
+  name: string = "";
+  message: string = "";
+  rating: string = "";
+  public Date: Date = new Date();
+     
+    messages: Message[] = [];
+    values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+     
+    addMessage(){
+        this.messages.push(new Message(this.name, this.message, this.rating));
+    }
 
   constructor() { }
 
