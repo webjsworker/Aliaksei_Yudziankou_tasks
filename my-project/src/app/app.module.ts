fbfd41ea48a22b783af67c172666ac4188433df6
Aliaksei <aliakseiyudziankou@gmail.com>
2021-12-13 23:41:40 +0300

    refactor: add style, del some files,

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -2,11 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ElemComponent } from './elem/elem.component';
-import { ElemsFilterPipe } from './shared/elems-filter.pipe';
-import { TextFormComponent } from './text-form/text-form.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
@@ -20,9 +16,6 @@ import { FeedbackComponent } from './feedback/feedback.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ElemComponent,
-    TextFormComponent,
-    ElemsFilterPipe,
     HeaderComponent,
     NavComponent,
     AboutComponent,
--- my-project/src/app/feedback/feedback.component.ts
+++ my-project/src/app/feedback/feedback.component.ts
@@ -19,15 +19,25 @@ export class FeedbackComponent implements OnInit {
   public Date: Date = new Date();
      
     messages: Message[] = [];
+
     values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
      
     addMessage(){
         this.messages.push(new Message(this.name, this.message, this.rating));
+        this.name = '';
+        this.message = '';
+        this.rating = '';
     }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.messages.push(new Message(this.name = " Mark", this.message = "It's great app, I've ever seen.", this.rating = "10"));
+    this.messages.push(new Message(this.name = " Lici", this.message = "I like using this app while I get home by underground. ", this.rating = "9"));
+    this.messages.push(new Message(this.name = " Борис", this.message = "Приятное приложене, нехватает выбора языка. ", this.rating = "7"));
+    this.name = '';
+    this.message = '';
+    this.rating = '';
   }
 
 }
